22704906570d4ff3ea808fdb0e0aed78c3c20d27
qianbin <cola.tin.com@gmail.com>
2018-12-24 00:29:10 +0800

    config workbox

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
-  lintOnSave: false,
-  baseUrl: process.env.NODE_ENV === 'production' ? '/insight-app/': '/'
+    lintOnSave: false,
+    baseUrl: process.env.NODE_ENV === 'production' ? '/insight-app/' : '/',
+    pwa: {
+        workboxOptions: {
+            skipWaiting: true,
+            clientsClaim: true
+        }
+    }
 }
