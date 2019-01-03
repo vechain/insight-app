641fa24923172d2b3c516f0a26c28518fe04ad0e
qianbin <cola.tin.com@gmail.com>
2019-01-03 16:11:31 +0800

    fix error without connex

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 
 export function isMainnet() {
-    return connex.thor.genesis.id === '0x00000000851caf3cfdb6e899cf5958bfb1ac3413d346d43539627e6be7ec1b4a'
+    return window.connex &&
+        connex.thor.genesis.id === '0x00000000851caf3cfdb6e899cf5958bfb1ac3413d346d43539627e6be7ec1b4a'
 }
 
