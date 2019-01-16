798c18ceb80341e39544d586786ab76e88d3699b
libotony <liboliqi@gmail.com>
2019-01-16 18:21:42 +0800

    set base url to root

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     lintOnSave: false,
-    baseUrl: process.env.NODE_ENV === 'production' ? '/insight/' : '/',
+    baseUrl: '/',
     pwa: {
         workboxOptions: {
             skipWaiting: true,
