f7a72204de0f9f9adfcf16e884db15f00e0f6f7c
dougalrea <dougalrea@users.noreply.github.com>
2024-05-15 14:07:48 +0000

    deploy: 54d2630d01c446a86ce8f5084a2c7119fd58a012

---
+++ precache-manifest.973b8dba51617352742c8d4b63c59a58.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f902f7e645697c3398f2",
+    "url": "css/app.62dc4395.css"
+  },
+  {
+    "revision": "f902f7e645697c3398f2",
+    "url": "js/app.0a374166.js"
+  },
+  {
+    "revision": "a6114d34e223b10d0c40",
+    "url": "js/chunk-2dde0b94.6b61171b.js"
+  },
+  {
+    "revision": "fb5a7772ed37c31f78f8",
+    "url": "js/chunk-vendors.cb281695.js"
+  },
+  {
+    "revision": "ff52cf4c77656aa0d750c944bb9bb4e6",
+    "url": "img/vechainstats-logo.ff52cf4c.png"
+  },
+  {
+    "revision": "3b2ae1f2edb3016c658970a383a974a0",
+    "url": "index.html"
+  },
+  {
+    "revision": "94950714edb823ff37d085ed03466498",
+    "url": "404.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+];
