b02a9fd7b0ae983f1c16c4d32e1beef64331cc1f
libotony <libotony@users.noreply.github.com>
2024-12-19 14:30:21 +0000

    deploy: 083a451345812addd06f56af4cc92a988beb2de1

---
+++ precache-manifest.f4540f21ad5735f301671d39c31536f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e97e83b8d6680790661",
+    "url": "css/app.62dc4395.css"
+  },
+  {
+    "revision": "3e97e83b8d6680790661",
+    "url": "js/app.711e7563.js"
+  },
+  {
+    "revision": "a6114d34e223b10d0c40",
+    "url": "js/chunk-2dde0b94.6b61171b.js"
+  },
+  {
+    "revision": "fb5a7772ed37c31f78f8",
+    "url": "js/chunk-vendors.cb281695.js"
+  },
+  {
+    "revision": "ff52cf4c77656aa0d750c944bb9bb4e6",
+    "url": "img/vechainstats-logo.ff52cf4c.png"
+  },
+  {
+    "revision": "4eefc830bd69bc79f6525da363ba62a7",
+    "url": "index.html"
+  },
+  {
+    "revision": "94950714edb823ff37d085ed03466498",
+    "url": "404.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+];
