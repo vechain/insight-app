7bb33a497b7ab0472c8acdf3c22d5a214c5bbd17
darrenvechain <darrenvechain@users.noreply.github.com>
2025-04-03 08:45:15 +0000

    deploy: 4c3d5599d96a7a3057cecb31617a4c554141513f

---
+++ precache-manifest.527834e23c750c4f750806a572bf8c65.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "993fb9f8d326ab4b72bb",
+    "url": "css/app.62dc4395.css"
+  },
+  {
+    "revision": "993fb9f8d326ab4b72bb",
+    "url": "js/app.05f678e2.js"
+  },
+  {
+    "revision": "a6114d34e223b10d0c40",
+    "url": "js/chunk-2dde0b94.6b61171b.js"
+  },
+  {
+    "revision": "fb5a7772ed37c31f78f8",
+    "url": "js/chunk-vendors.cb281695.js"
+  },
+  {
+    "revision": "ff52cf4c77656aa0d750c944bb9bb4e6",
+    "url": "img/vechainstats-logo.ff52cf4c.png"
+  },
+  {
+    "revision": "32a1290d5428d40dde17c2e1201a427d",
+    "url": "index.html"
+  },
+  {
+    "revision": "94950714edb823ff37d085ed03466498",
+    "url": "404.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+];
